dd6d4944ac300a9f068d8fe1f05cf59fca832270
LadyLiza <elizabet132010@gmail.com>
2025-07-23 21:59:11 +0200

    add task solution

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,32 +1,64 @@
 /// <reference types="cypress" />
 /// <reference types="../support" />
 
-describe('Settings page', () => {
-  before(() => {
+import { SettingsPage } from '../support/pages/SettingsPage';
 
-  });
+describe('Settings page', () => {
+  const settingsPage = new SettingsPage();
+  let email;
 
   beforeEach(() => {
-
+    const timestamp = Date.now();
+    email = `liza${timestamp}@gmail.com`;
+    cy.request('POST', 'http://localhost:3000/api/users', {
+      user: {
+        username: `liza${timestamp}`,
+        email,
+        password: '1234567Qwerty',
+        bio: 'Test user bio',
+      }
+    });
+    cy.visit('/user/login');
+    cy.get('input[placeholder="Email"]').type(email);
+    cy.get('input[placeholder="Password"]').type('1234567Qwerty');
+    cy.contains('button', 'Sign in').click();
+    cy.location('pathname', { timeout: 10000 }).should('not.include', '/login');
+    settingsPage.visit();
   });
 
-  it('should provide an ability to update username', () => {
-
+  it('should update username', () => {
+    settingsPage.getUsernameInput().clear().type('superQA_user');
+    settingsPage.getEmailInput().clear().type(email);
+    settingsPage.getPasswordInput().clear().type('1234567Qwerty');
+    settingsPage.getUpdateButton().click();
   });
 
-  it('should provide an ability to update bio', () => {
-
+  it('should update bio', () => {
+    settingsPage.getBioTextarea().clear().type('I am the king of Cypress!');
+    settingsPage.getUsernameInput().clear().type('superQA_user');
+    settingsPage.getEmailInput().clear().type(email);
+    settingsPage.getPasswordInput().clear().type('1234567Qwerty');
+    settingsPage.getUpdateButton().click();
   });
 
-  it('should provide an ability to update an email', () => {
-
+  it('should update email', () => {
+    settingsPage.getEmailInput().clear().type(`superqa_test_${Date.now()}@gmail.com`);
+    settingsPage.getUsernameInput().clear().type('superQA_user');
+    settingsPage.getPasswordInput().clear().type('1234567Qwerty');
+    settingsPage.getUpdateButton().click();
   });
 
-  it('should provide an ability to update password', () => {
-
+  it('should update password', () => {
+    settingsPage.getPasswordInput().type('MegaSecret123!');
+    settingsPage.getUsernameInput().clear().type('superQA_user');
+    settingsPage.getEmailInput().clear().type(email);
+    settingsPage.getUpdateButton().click();
   });
 
-  it('should provide an ability to log out', () => {
+  it('should log out', () => {
+    settingsPage.getLogoutButton().click();
 
+    cy.url({ timeout: 10000 }).should('eq', 'http://localhost:3000/');
+    cy.contains('a', 'Sign in').should('be.visible');
   });
 });
